731b10208fbbcfd0cd8bbcf42019d7b1b336afad
nosopirochka <dashima.1998@mail.ru>
2024-05-16 18:56:56 +0300

    Задание 3. Добавлены логи

--- src/main/java/com/gpb/minibank/service/TelegramBot.java
+++ src/main/java/com/gpb/minibank/service/TelegramBot.java
@@ -3,13 +3,14 @@ package com.gpb.minibank.service;
 import com.gpb.minibank.service.commandMaker.CommandMaker;
 import com.gpb.minibank.service.messageCreater.MessageCreater;
 import com.gpb.minibank.service.messageSender.MessageSender;
+import lombok.extern.slf4j.Slf4j;
 import org.jvnet.hk2.annotations.Service;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-
+@Slf4j
 @Service
 public class TelegramBot extends TelegramLongPollingBot {
 
@@ -34,14 +35,22 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+            log.info("! Получено сообщение ! ");
+            var command = update.getMessage().getText();
             // выполняем и получаем результат выбранной пользователем команды
+            log.info("Пробую выполнить команду '{}'", command);
             var resultOfCommand = CommandMaker.work(update);
+            log.info("Команда '{}' выполнена!", command);
             // создаём ответ
+            log.info("Создаю ответ.");
             var message = MessageCreater.createMessage(update.getMessage().getChatId(), resultOfCommand);
             try {
                 // отправляем ответ пользователю
+                log.info("Отправляю ответ!");
                 new MessageSender(this.botToken, message).sendMessage();
+                log.info("Ответ успешно отправлен.");
             } catch (TelegramApiException e) {
+                log.error("Произошла ошибка во время отправки ответа '{}'", e.getMessage());
                 System.out.print(e.getMessage());
             }
         }
