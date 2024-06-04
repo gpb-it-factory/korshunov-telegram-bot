46f9188d20c2b92f70666505c9caace268a1e176
nosopirochka <dashima.1998@mail.ru>
2024-06-04 16:08:15 +0300

    Созданы условия для соблюдения open-closed принципа

--- src/main/java/com/gpb/minibank/service/commandMaker/CommandMaker.java
+++ src/main/java/com/gpb/minibank/service/commandMaker/CommandMaker.java
@@ -1,47 +1,31 @@
 package com.gpb.minibank.service.commandMaker;
 
-import com.gpb.minibank.service.commandMaker.commands.Ping;
-import com.gpb.minibank.service.commandMaker.commands.Register;
-import com.gpb.minibank.service.commandMaker.commands.Start;
+import com.gpb.minibank.service.commandMaker.commands.Command;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import java.util.List;
 
 @Slf4j
 @Component
-public class CommandMaker {
+public final class CommandMaker {
 
-    private final Start start;
+    private final List<Command> commandList;
 
-    private final Ping ping;
-
-    private final Register register;
-
-    public CommandMaker(Start start, Ping ping, Register register) {
-        this.start = start;
-        this.ping = ping;
-        this.register = register;
+    public CommandMaker(List<Command> commandList) {
+        this.commandList = commandList;
     }
 
     public String work(Update update) {
-        var commandString = update.getMessage().getText();
-        return switch (commandString) {
-            case "/start":
-                writeLog(commandString);
-                yield start.exec(update);
-            case "/ping":
-                writeLog(commandString);
-                yield ping.exec();
-            case "/register":
-                writeLog(commandString);
-                yield register.exec(update);
-            default:
-                log.info("Получена неизвестная команда '{}'!", commandString);
-                yield String.format("Я не умею выполнять команду '%s'!", commandString);
-        };
-    }
-
-    public void writeLog(String commandString) {
-        log.info("Выполняю команду {}!", commandString);
+        var commandFromUser = update.getMessage().getText();
+        var command = commandList.stream()
+                .filter(someCommand -> someCommand.getNameOfCommand().equals(commandFromUser))
+                .findFirst();
+        if (command.isPresent()) {
+            log.info("Выполняю команду {}!", commandFromUser);
+            return command.get().exec(update);
+        }
+        log.info("Получена неизвестная команда '{}'!", commandFromUser);
+        return String.format("Я не умею выполнять команду '%s'!", commandFromUser);
     }
 }
