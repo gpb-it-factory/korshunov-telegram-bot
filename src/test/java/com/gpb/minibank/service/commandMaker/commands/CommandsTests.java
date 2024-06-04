f9094175608e55500cb0d71986245ebc12fc2d2d
nosopirochka <dashima.1998@mail.ru>
2024-06-04 16:08:39 +0300

    Задание 6. Добавлены тесты

--- src/test/java/com/gpb/minibank/service/commandMaker/commands/CommandsTests.java
+++ src/test/java/com/gpb/minibank/service/commandMaker/commands/CommandsTests.java
@@ -1,13 +1,30 @@
 package com.gpb.minibank.service.commandMaker.commands;
 
+import com.gpb.minibank.service.commandMaker.commands.requestRunner.RegisterRequestRunner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@ExtendWith(MockitoExtension.class)
 public class CommandsTests {
+
+    @Mock
+    public RegisterRequestRunner registerRequestRunner;
+
+    @InjectMocks
+    public Register register;
 
     static Update updateResult;
 
@@ -21,6 +38,7 @@ public class CommandsTests {
     public static void createUpdate() {
         var chat = new Chat();
         chat.setFirstName("Василий");
+        chat.setLinkedChatId(1000L);
         var message = new Message();
         message.setChat(chat);
         var update = new Update();
@@ -39,8 +57,34 @@ public class CommandsTests {
     @Test
     void testPing() {
         updateResult.getMessage().setText("/ping");
-        var result = new Ping().exec();
+        var result = new Ping().exec(updateResult);
 
         Assertions.assertEquals("pong", result);
+    }
+
+    @Test
+    void testRegister() {
+        Mockito.doReturn(ResponseEntity.status(HttpStatus.NO_CONTENT).build())
+                .when(registerRequestRunner).runRequest(updateResult);
+        var result = register.exec(updateResult);
+        Assertions.assertEquals("Вы успешно зарегистрированы!", result);
+    }
+
+    @Test
+    void testRegisterWithRestException() {
+        Mockito.doThrow(RestClientException.class)
+                .when(registerRequestRunner).runRequest(updateResult);
+        var result = register.exec(updateResult);
+
+        Assertions.assertEquals("Сервис не доступен!", result);
+    }
+
+    @Test
+    void testRegisterWithExceptionFromMiddle() {
+        Mockito.when(registerRequestRunner.runRequest(updateResult))
+                .thenThrow(new HttpStatusCodeException(HttpStatus.BAD_REQUEST) {});
+        var result = register.exec(updateResult);
+
+        Assertions.assertEquals("Произошла ошибка.\n", result);
     }
 }
