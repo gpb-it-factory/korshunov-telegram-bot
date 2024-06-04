d63f8e08ec6681f1425c1f5c4129f31bbae7e671
nosopirochka <dashima.1998@mail.ru>
2024-06-04 16:06:37 +0300

    Задание 6. Добавлена абстракция для запроса на middle

---
+++ src/main/java/com/gpb/minibank/service/commandMaker/commands/requestRunner/RegisterRequestRunner.java
@@ -0,0 +1,29 @@
+package com.gpb.minibank.service.commandMaker.commands.requestRunner;
+
+import com.gpb.minibank.service.commandMaker.commands.dto.request.CreateUserDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public final class RegisterRequestRunner {
+
+    private final RestTemplate restTemplate;
+    private final String path;
+
+    public RegisterRequestRunner(RestTemplateBuilder restTemplateBuilder,
+                                 @Value("${bot.service.register.path}") String path) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.path = path;
+    }
+
+    public ResponseEntity<String> runRequest(Update update) {
+        var id = update.getMessage().getChatId();
+        HttpEntity<CreateUserDTO> entity = new HttpEntity<>(new CreateUserDTO(id));
+        return restTemplate.postForEntity(path, entity, String.class);
+    }
+}
