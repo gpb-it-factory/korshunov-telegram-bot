c5faeb50d6078930d3a0ee7c1aa7ad28665161dc
nosopirochka <dashima.1998@mail.ru>
2024-06-18 00:25:52 +0300

    Задание 7. Добавлена валидация полей

--- src/main/java/com/gpb/minibank/service/commandHandler/commands/dto/request/CreateAccountDTO.java
+++ src/main/java/com/gpb/minibank/service/commandHandler/commands/dto/request/CreateAccountDTO.java
@@ -1,5 +1,8 @@
 package com.gpb.minibank.service.commandHandler.commands.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -9,8 +12,11 @@ import lombok.Getter;
 @EqualsAndHashCode
 public class CreateAccountDTO {
 
+    @NotNull
+    @Positive
     private Long userId;
 
+    @NotBlank
     private String accountName;
 
 }
